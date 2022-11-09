a731fd6ee67c3937a1d27215fbef0bc011f6522d
LPenjin <l.penjo@gmail.com>
2022-11-09 12:34:08 +0100

    adds more atributes to clan and makes readme more verbose

--- kset_clanovi.sql
+++ kset_clanovi.sql
@@ -26,11 +26,15 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.clan (
     datumuclanjenja date NOT NULL,
-    sifra_plave character(5) NOT NULL,
-    imeprezime character(100) NOT NULL,
+    sifraplave character(5) NOT NULL,
     imanarancastu boolean NOT NULL,
     pocasnoclanstvo boolean NOT NULL,
-    clanstvovrijedido date NOT NULL
+    clanstvovrijedido date NOT NULL,
+    datrod date NOT NULL,
+    imeclan character varying(50) NOT NULL,
+    prezimeclan character varying(50) NOT NULL,
+    kandpoc boolean NOT NULL,
+    predpotstud boolean NOT NULL
 );
 
 
@@ -41,8 +45,8 @@ ALTER TABLE public.clan OWNER TO postgres;
 --
 
 CREATE TABLE public.jediouprave (
-    pozicija character(20) NOT NULL,
-    sifra_plave character(5) NOT NULL,
+    pozicija character varying(20) NOT NULL,
+    sifraplave character(5) NOT NULL,
     godina integer NOT NULL
 );
 
@@ -54,9 +58,9 @@ ALTER TABLE public.jediouprave OWNER TO postgres;
 --
 
 CREATE TABLE public.sekcija (
-    ime character(20) NOT NULL,
-    pocetaksastanka character(10) NOT NULL,
-    prostor character(20) NOT NULL,
+    ime character varying(20) NOT NULL,
+    pocetaksastanka character(5) NOT NULL,
+    prostor character varying(20) NOT NULL,
     sef character(5) NOT NULL
 );
 
@@ -68,8 +72,8 @@ ALTER TABLE public.sekcija OWNER TO postgres;
 --
 
 CREATE TABLE public.uclanjen_u (
-    sifra_plave character(5) NOT NULL,
-    ime character(20) NOT NULL
+    sifraplave character(5) NOT NULL,
+    ime character varying(20) NOT NULL
 );
 
 
@@ -81,7 +85,7 @@ ALTER TABLE public.uclanjen_u OWNER TO postgres;
 
 CREATE TABLE public.uprava (
     godina integer NOT NULL,
-    pocetaksastanka character(10) NOT NULL
+    pocetaksastanka character(5) NOT NULL
 );
 
 
@@ -91,37 +95,37 @@ ALTER TABLE public.uprava OWNER TO postgres;
 -- Data for Name: clan; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.clan (datumuclanjenja, sifra_plave, imeprezime, imanarancastu, pocasnoclanstvo, clanstvovrijedido) FROM stdin;
-2022-10-01	AA-20	Pero Peric                                                                                          	f	f	2023-10-01
-2022-10-01	AA-21	Ana Anic                                                                                            	f	f	2023-10-01
-2022-10-01	AA-22	Fran Franic                                                                                         	f	f	2023-10-01
-2022-10-01	AA-23	Luka Lukic                                                                                          	f	f	2023-10-01
-2022-10-01	AA-24	Toni Tonic                                                                                          	f	f	2023-10-01
-2022-10-01	AA-25	Klara Klaric                                                                                        	f	f	2023-10-01
-2022-10-01	AA-26	Bartol Bartolic                                                                                     	f	f	2023-10-01
-2022-10-01	AA-27	Zaga Zagic                                                                                          	f	f	2023-10-01
-2022-10-01	AA-28	Karla Karlic                                                                                        	f	f	2023-10-01
-2022-10-01	AA-29	Drago Dragic                                                                                        	f	f	2023-10-01
-2022-10-01	AA-30	Koka Kokic                                                                                          	f	f	2023-10-01
-2022-10-01	AA-31	Alex Turner                                                                                         	f	f	2023-10-01
-2022-10-01	AA-32	Andro Andric                                                                                        	f	f	2023-10-01
-2022-10-01	AA-33	Antonio Antonic                                                                                     	f	f	2023-10-01
-2022-10-01	AA-35	Ivan Ivic                                                                                           	f	f	2023-10-01
-2022-10-01	AA-36	Disko Čiča                                                                                          	f	f	2023-10-01
-2022-10-01	AA-37	Sef Sefic                                                                                           	t	f	2023-10-01
-2022-10-01	AA-38	Stevo Blagajnik                                                                                     	t	f	2023-10-01
-2022-10-01	AA-39	Marko Tajnik                                                                                        	t	f	2023-10-01
-2022-10-01	AA-40	Marko Markic                                                                                        	t	f	2023-10-01
-2022-10-01	AA-41	Josip Josipovic                                                                                     	t	f	2023-10-01
-2022-10-01	AA-42	Nikola Nikolic                                                                                      	t	f	2023-10-01
-2022-10-01	AA-43	Marin Marinic                                                                                       	t	f	2023-10-01
-2022-10-01	AA-44	Patrik Patrikic                                                                                     	t	f	2023-10-01
-2022-10-01	AA-45	Juraj Juric                                                                                         	t	f	2023-10-01
-2022-10-01	AA-46	Helena Helenic                                                                                      	t	f	2023-10-01
-2022-10-01	AA-47	Arnold Schwarzenger                                                                                 	t	f	2023-10-01
-2022-10-01	AA-48	Silvester Stalone                                                                                   	t	f	2023-10-01
-2022-10-01	AA-49	Paolo Paolic                                                                                        	t	f	2023-10-01
-2022-10-01	AA-50	Tamara Tamaric                                                                                      	t	f	2023-10-01
+COPY public.clan (datumuclanjenja, sifraplave, imanarancastu, pocasnoclanstvo, clanstvovrijedido, datrod, imeclan, prezimeclan, kandpoc, predpotstud) FROM stdin;
+2022-10-01	AA-20	f	f	2023-10-01	2001-10-01	Pero	Peric	f	t
+2022-10-01	AA-21	f	f	2023-10-01	2002-10-01	Ana	Anic	f	t
+2022-10-01	AA-22	f	f	2023-10-01	1995-10-01	Fran	Franic	f	t
+2022-10-01	AA-23	f	f	2023-10-01	2001-10-01	Luka	Lukic	f	t
+2022-10-01	AA-24	f	f	2023-10-01	1998-10-01	Toni	Tonic	f	t
+2022-10-01	AA-25	f	f	2023-10-01	2000-10-01	Klara	Klaric	f	t
+2022-10-01	AA-26	f	f	2023-10-01	2004-10-01	Bartol	Bartolic	f	t
+2022-10-01	AA-27	f	f	2023-10-01	1999-10-01	Zaga	Zagic	f	t
+2022-10-01	AA-28	f	f	2023-10-01	2000-10-01	Karla	Karlic	f	t
+2022-10-01	AA-29	f	f	2023-10-01	2000-10-01	Drago	Dragic	f	t
+2022-10-01	AA-30	f	f	2023-10-01	2000-10-01	Koka	Kokic	f	t
+2022-10-01	AA-31	f	f	2023-10-01	2000-10-01	Alex	Turner	f	t
+2022-10-01	AA-32	f	f	2023-10-01	2000-10-01	Andro	Andric	f	t
+2022-10-01	AA-33	f	f	2023-10-01	2000-10-01	Antonio	Antonic	f	t
+2022-10-01	AA-35	f	f	2023-10-01	2000-10-01	Ivan	Ivic	f	t
+2022-10-01	AA-36	f	f	2023-10-01	2000-10-01	Disko	Čiča	f	t
+2022-10-01	AA-37	t	f	2023-10-01	2000-10-01	Sef	Sefic	f	t
+2022-10-01	AA-38	t	f	2023-10-01	2000-10-01	Stevo	Blagajnik	f	t
+2022-10-01	AA-39	t	f	2023-10-01	2000-10-01	Marko	Tajnik	f	t
+2022-10-01	AA-40	t	f	2023-10-01	2000-10-01	Marko	Markic	f	t
+2022-10-01	AA-41	f	t	2023-10-01	2000-10-01	Josip 	Josipovic	f	t
+2022-10-01	AA-42	t	f	2023-10-01	2000-10-01	Nikola	Nikolic	f	t
+2022-10-01	AA-43	t	f	2023-10-01	2000-10-01	Marin	Marinic	f	t
+2022-10-01	AA-44	t	f	2023-10-01	2000-10-01	Patrik	Patrikic	f	t
+2022-10-01	AA-45	t	f	2023-10-01	2000-10-01	Juraj	Juric	f	t
+2022-10-01	AA-46	t	f	2023-10-01	2000-10-01	Helena	Helenic	f	t
+2022-10-01	AA-47	t	f	2023-10-01	2000-10-01	Arnold	Schwarzeneger	t	f
+2022-10-01	AA-48	f	t	2023-10-01	2000-10-01	Silvester	Stalone	f	t
+2022-10-01	AA-49	t	f	2023-10-01	2000-10-01	Paolo	Paolic	t	f
+2022-10-01	AA-50	t	f	2023-10-01	2000-10-01	Tamara	Tamaric	f	t
 \.
 
 
@@ -129,10 +133,10 @@ COPY public.clan (datumuclanjenja, sifra_plave, imeprezime, imanarancastu, pocas
 -- Data for Name: jediouprave; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.jediouprave (pozicija, sifra_plave, godina) FROM stdin;
-Sef                 	AA-37	2022
-Blagajnik           	AA-38	2022
-Tajnik              	AA-39	2022
+COPY public.jediouprave (pozicija, sifraplave, godina) FROM stdin;
+Sef	AA-37	2022
+Blagajnik	AA-38	2022
+Tajnik	AA-39	2022
 \.
 
 
@@ -141,15 +145,15 @@ Tajnik              	AA-39	2022
 --
 
 COPY public.sekcija (ime, pocetaksastanka, prostor, sef) FROM stdin;
-Bike sekcija        	18:00     	Tech prostor        	AA-35
-Disko sekcija       	18:00     	Kabina              	AA-24
-Foto sekcija        	18:00     	Foto prostorija     	AA-50
-Comp sekcija        	18:00     	Comp Prostorija     	AA-41
-Video sekcija       	18:00     	Video Prostorija    	AA-44
-Tech sekcija        	18:00     	Tech Prostorija     	AA-43
-Glazbena sekcija    	18:00     	Glazbena prostorija 	AA-48
-Planinarska sekcija 	18:00     	Tech prostorija     	AA-46
-Dramska sekcija     	18:00     	Ispod stepenica     	AA-42
+Bike sekcija	18:00	Tech prostor	AA-35
+Disko sekcija	18:00	Kabina	AA-24
+Foto sekcija	18:00	Foto prostorija	AA-50
+Comp sekcija	18:00	Comp Prostorija	AA-41
+Video sekcija	18:00	Video Prostorija	AA-44
+Tech sekcija	18:00	Tech Prostorija	AA-43
+Glazbena sekcija	18:00	Glazbena prostorija	AA-48
+Planinarska sekcija	18:00	Tech prostorija	AA-46
+Dramska sekcija	18:00	Ispod stepenica	AA-42
 \.
 
 
@@ -157,40 +161,40 @@ Dramska sekcija     	18:00     	Ispod stepenica     	AA-42
 -- Data for Name: uclanjen_u; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.uclanjen_u (sifra_plave, ime) FROM stdin;
-AA-20	Bike sekcija        
-AA-35	Bike sekcija        
-AA-24	Disko sekcija       
-AA-42	Dramska sekcija     
-AA-50	Foto sekcija        
-AA-48	Glazbena sekcija    
-AA-48	Planinarska sekcija 
-AA-41	Comp sekcija        
-AA-44	Video sekcija       
-AA-43	Tech sekcija        
-AA-21	Disko sekcija       
-AA-20	Disko sekcija       
-AA-22	Disko sekcija       
-AA-23	Disko sekcija       
-AA-25	Disko sekcija       
-AA-26	Disko sekcija       
-AA-27	Tech sekcija        
-AA-28	Tech sekcija        
-AA-29	Tech sekcija        
-AA-30	Tech sekcija        
-AA-31	Video sekcija       
-AA-33	Video sekcija       
-AA-36	Bike sekcija        
-AA-37	Bike sekcija        
-AA-38	Bike sekcija        
-AA-39	Dramska sekcija     
-AA-40	Comp sekcija        
-AA-42	Comp sekcija        
-AA-45	Comp sekcija        
-AA-46	Comp sekcija        
-AA-47	Comp sekcija        
-AA-49	Comp sekcija        
-AA-46	Planinarska sekcija 
+COPY public.uclanjen_u (sifraplave, ime) FROM stdin;
+AA-20	Bike sekcija
+AA-35	Bike sekcija
+AA-24	Disko sekcija
+AA-42	Dramska sekcija
+AA-50	Foto sekcija
+AA-48	Glazbena sekcija
+AA-48	Planinarska sekcija
+AA-41	Comp sekcija
+AA-44	Video sekcija
+AA-43	Tech sekcija
+AA-21	Disko sekcija
+AA-20	Disko sekcija
+AA-22	Disko sekcija
+AA-23	Disko sekcija
+AA-25	Disko sekcija
+AA-26	Disko sekcija
+AA-27	Tech sekcija
+AA-28	Tech sekcija
+AA-29	Tech sekcija
+AA-30	Tech sekcija
+AA-31	Video sekcija
+AA-33	Video sekcija
+AA-36	Bike sekcija
+AA-37	Bike sekcija
+AA-38	Bike sekcija
+AA-39	Dramska sekcija
+AA-40	Comp sekcija
+AA-42	Comp sekcija
+AA-45	Comp sekcija
+AA-46	Comp sekcija
+AA-47	Comp sekcija
+AA-49	Comp sekcija
+AA-46	Planinarska sekcija
 \.
 
 
@@ -199,7 +203,7 @@ AA-46	Planinarska sekcija
 --
 
 COPY public.uprava (godina, pocetaksastanka) FROM stdin;
-2022	19:00     
+2022	19:00
 \.
 
 
@@ -208,7 +212,7 @@ COPY public.uprava (godina, pocetaksastanka) FROM stdin;
 --
 
 ALTER TABLE ONLY public.clan
-    ADD CONSTRAINT clan_pkey PRIMARY KEY (sifra_plave);
+    ADD CONSTRAINT clan_pkey PRIMARY KEY (sifraplave);
 
 
 --
@@ -216,7 +220,7 @@ ALTER TABLE ONLY public.clan
 --
 
 ALTER TABLE ONLY public.jediouprave
-    ADD CONSTRAINT jediouprave_pkey PRIMARY KEY (sifra_plave, godina);
+    ADD CONSTRAINT jediouprave_pkey PRIMARY KEY (sifraplave, godina);
 
 
 --
@@ -232,7 +236,7 @@ ALTER TABLE ONLY public.sekcija
 --
 
 ALTER TABLE ONLY public.uclanjen_u
-    ADD CONSTRAINT uclanjen_u_pkey PRIMARY KEY (sifra_plave, ime);
+    ADD CONSTRAINT uclanjen_u_pkey PRIMARY KEY (sifraplave, ime);
 
 
 --
@@ -256,7 +260,7 @@ ALTER TABLE ONLY public.jediouprave
 --
 
 ALTER TABLE ONLY public.jediouprave
-    ADD CONSTRAINT jediouprave_sifra_plave_fkey FOREIGN KEY (sifra_plave) REFERENCES public.clan(sifra_plave);
+    ADD CONSTRAINT jediouprave_sifra_plave_fkey FOREIGN KEY (sifraplave) REFERENCES public.clan(sifraplave);
 
 
 --
@@ -264,7 +268,7 @@ ALTER TABLE ONLY public.jediouprave
 --
 
 ALTER TABLE ONLY public.sekcija
-    ADD CONSTRAINT sekcija_sef_fkey FOREIGN KEY (sef) REFERENCES public.clan(sifra_plave);
+    ADD CONSTRAINT sekcija_sef_fkey FOREIGN KEY (sef) REFERENCES public.clan(sifraplave);
 
 
 --
@@ -280,7 +284,7 @@ ALTER TABLE ONLY public.uclanjen_u
 --
 
 ALTER TABLE ONLY public.uclanjen_u
-    ADD CONSTRAINT uclanjen_u_sifra_plave_fkey FOREIGN KEY (sifra_plave) REFERENCES public.clan(sifra_plave);
+    ADD CONSTRAINT uclanjen_u_sifra_plave_fkey FOREIGN KEY (sifraplave) REFERENCES public.clan(sifraplave);
 
 
 --
