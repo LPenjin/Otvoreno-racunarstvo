353c9bc7119bd8452fcd26616f06b923fc2a811e
LPenjin <l.penjo@gmail.com>
2023-01-04 14:57:13 +0100

    Solves 4. lab

--- otvoreno_racunarstvo/__init__.py
+++ otvoreno_racunarstvo/__init__.py
@@ -3,10 +3,13 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '3d951e864422a3d0fc27b496b06a5c9f'
+app.secret_key = '3d951e864422a3d0fc27b496b06a5c9f'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localHost:5432/Otvoreno_racunarstvo"
 app.config['PATH'] = os.path.abspath('..').replace('\\', '/')
+app.config['AUTH0_CLIENT_ID'] = '0F6lPPCGlAimdbq03B1MyPKROlDVFj1i'
+app.config['AUTH0_CLIENT_SECRET'] = '5Nvyjy5NgCdG5SWSZa6VWRgPi8_pX3QQ38pngOg7Z5v1UACAcqODfa5T0WtMoyaX '
+app.config['AUTH0_DOMAIN'] = 'dev-8nfqfpbzo0kif14w.us.auth0.com'
 db = SQLAlchemy(app)
 
 from otvoreno_racunarstvo import routes
--- otvoreno_racunarstvo/routes.py
+++ otvoreno_racunarstvo/routes.py
@@ -1,6 +1,26 @@
 from otvoreno_racunarstvo import app, db
-from flask import render_template, send_file, jsonify, request, make_response, json
+from flask import render_template, send_file, jsonify, request, make_response, json, redirect, session, url_for
+from authlib.integrations.flask_client import OAuth
+from urllib.parse import quote_plus, urlencode
+from dotenv import find_dotenv, load_dotenv
+from os import environ as env
 import os
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
 
 statement = "select imeclan, prezimeclan, sifraplave, imanarancastu, pocasnoclanstvo, datrod, kandpoc, predpotstud, datumuclanjenja, clanstvovrijedido, ime, prostor, pocetaksastanka from clan natural join uclanjen_u natural join sekcija"
 
@@ -31,11 +51,31 @@ def to_dict_sekcija(sekcija):
     dict["pocetak_sastanka"] = sekcija._data[1]
     dict["prostor"] = sekcija._data[2]
     dict["sef_sekcije"] = sekcija._data[3]
+    if len(sekcija._data) > 4:
+        dict["imeclan"] = sekcija._data[10]
+        dict["prezimeclan"] = sekcija._data[11]
+        dict["sifraplave"] = sekcija._data[5]
+        dict["imanarancastu"] = sekcija._data[6]
+        dict["pocasnoclanstvo"] = sekcija._data[7]
+        dict["datrod"] = sekcija._data[9]
+        dict["kandpoc"] = sekcija._data[12]
+        dict["predpotstud"] = sekcija._data[13]
+        dict["datumuclanjenja"] = sekcija._data[4]
+        dict["clanstvovrijedido"] = sekcija._data[8]
+
     return dict
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="index")
+    clanovi = db.engine.execute(statement)
+    ksetovci = [to_dict(clan) for clan in clanovi]
+    sekcije = db.engine.execute("select * from sekcija left join clan on sifraplave=sef")
+    sekcije = [to_dict_sekcija(sekcija) for sekcija in sekcije]
+    return render_template("index.html",
+                           session=session.get('user'),
+                           pretty=json.dumps(session.get('user'), indent=4),
+                           ksetovac=ksetovci[0],
+                           sekcija=sekcije[0])
 
 @app.route('/download_csv')
 def download():
@@ -67,7 +107,7 @@ def get_clanovi_po_plavoj():
     if id:=args.get('id'):
         clanovi = db.engine.execute(statement_id + id + "\'")
         data = {"data": [to_dict(clan) for clan in clanovi]}
-        if not data:
+        if not data['data']:
             return jsonify({"error": "Clan not found"}), 404
         return jsonify(data), 200
     else:
@@ -91,7 +131,7 @@ def get_sekcije_by_prostor():
     if prostor := args.get('prostor'):
         sekcije = db.engine.execute(f"select * from sekcija where prostor like '{prostor}'")
         data = {"data": [to_dict_sekcija(sekcija) for sekcija in sekcije]}
-        if not data:
+        if not data['data']:
             return jsonify({"error": "Clan not found"}), 404
         return jsonify(data), 200
     else:
@@ -105,7 +145,7 @@ def get_openapi():
     data = json.load(open(r'C:\Users\lpenj\Desktop\Very important stuff\Fakultetlaz\7. semestar\Otvoreno racunarstvo\Otvoreno-racunarstvo\otvoreno_racunarstvo\openapi.json'))
     return make_response(data, 200)
 
-@app.route('/clan/post', methods=['POST'])
+@app.route('/clan/post', methods=['POST']   )
 def post_clan():
     datumuclanjenja = request.form['datumuclanjenja']
     sifraplave = request.form['sifraplave']
@@ -117,10 +157,12 @@ def post_clan():
     prezimeclan = request.form['prezimeclan']
     kandpoc = request.form['kandpoc']
     predpotstud = request.form['predpotstud']
+    sekcija = request.form['sekcija']
     db.engine.execute(statement_post + f"(\'{datumuclanjenja}\', \'{sifraplave}\', {imanarancastu}, {pocasnoclanstvo}, "
                                        f"\'{clanstvovrijedido}\', \'{datrod}\', \'{imeclan}\', \'{prezimeclan}\', "
                                        f"{kandpoc}, {predpotstud})")
-    return make_response("Success", 200)
+    db.engine.execute(f"insert into uclanjen_u(sifraplave, ime) values(\'{sifraplave}\', \'{sekcija}\')")
+    return jsonify({"status": "Success"}), 200
 
 
 @app.route('/clan/put', methods=['PUT'])
@@ -135,17 +177,50 @@ def update_clan():
     prezimeclan = request.form['prezimeclan']
     kandpoc = request.form['kandpoc']
     predpotstud = request.form['predpotstud']
+    sekcija = request.form['sekcija']
     db.engine.execute("update clan set " + f"datumuclanjenja = \'{datumuclanjenja}\', imanarancastu={imanarancastu}, "
                                       f"pocasnoclanstvo={pocasnoclanstvo}, clanstvovrijedido=\'{clanstvovrijedido}\', "
                                         f"datrod=\'{datrod}\', imeclan=\'{imeclan}\', prezimeclan=\'{prezimeclan}\', "
                                       f"kandpoc={kandpoc}, predpotstud={predpotstud} "
                                       f"where sifraplave like \'{sifraplave}\'")
-    return make_response("Success", 200)
+    db.engine.execute(f"update uclanjen_u set ime=\'{sekcija}\'"
+                      f"where sifraplave like \'{sifraplave}\'")
+    return jsonify({"status": "Success"}), 200
 
 
 @app.route('/clan/delete', methods=['DELETE'])
 def delete_clan():
     sifraplave = request.form['sifraplave']
+    db.engine.execute(f"delete from uclanjen_u where sifraplave=\'{sifraplave}\'")
     db.engine.execute(f"delete from clan where sifraplave=\'{sifraplave}\'")
-    return make_response("Success", 200)
+    return jsonify({"status": "Success"}), 200
 
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
